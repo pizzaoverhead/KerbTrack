e4394b35a3db95ec838cd827319e2244811b14d2
pizzaoverhead <pizzaoverhead@gmail.com>
2016-10-24 21:23:57 +0100

    Missed in previous commit.

--- KerbTrack/Properties/AssemblyInfo.cs
+++ KerbTrack/Properties/AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("KerbTrack")]
-[assembly: AssemblyCopyright("Copyright © pizzaoverhead 2014")]
+[assembly: AssemblyCopyright("Copyright © pizzaoverhead 2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -36,5 +36,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.2.0.0")]
-[assembly: AssemblyFileVersion("1.2.0.0")]
+[assembly: AssemblyVersion("1.3.0.0")]
+[assembly: AssemblyFileVersion("1.3.0.0")]
